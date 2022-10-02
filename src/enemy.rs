eea751600ed41c46e29bad65cc9892553e643a4e
Pietrek14 <dawidekpe@gmail.com>
2022-10-02 06:09:23 +0200

    the enemy moves, but doesn't avoid walls

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,4 +1,4 @@
-use crate::health_bar::HealthBar;
+use crate::game_area::{grid_to_world_coords, world_to_grid_coords};
 use crate::loading::TextureAssets;
 use crate::GameState;
 use crate::{
@@ -7,6 +7,7 @@ use crate::{
 };
 use bevy::math::Vec3Swizzles;
 use bevy::{prelude::*, sprite::collide_aabb::collide};
+use pathfinding::prelude::{astar, Grid};
 
 pub struct EnemyPlugin;
 
@@ -16,7 +17,8 @@ impl Plugin for EnemyPlugin {
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
                     .with_system(hit_player)
-                    .with_system(follow_player),
+                    .with_system(follow_player.label("enemy_ai"))
+                    .with_system(update_enemy_position.after("enemy_ai")),
             )
             .add_system_set(SystemSet::on_exit(GameState::Playing).with_system(drop_enemies));
     }
@@ -28,56 +30,136 @@ struct Enemy;
 #[derive(Component)]
 struct AttackTimer(Timer);
 
+#[derive(Component)]
+struct PathCache {
+    player_square: (usize, usize),
+    path: Option<Vec<Vec2>>,
+    index: usize,
+}
+
 #[derive(Bundle)]
 pub struct EnemyBundle {
     health: Health,
     movement: Movement,
     enemy: Enemy,
     attack_timer: AttackTimer,
+    path_cache: PathCache,
     #[bundle]
     sprite: SpriteBundle,
 }
 
 fn follow_player(
     player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
-    mut enemies_query: Query<(&mut Transform, &Movement), With<Enemy>>,
+    mut enemies_query: Query<(&mut Transform, &mut PathCache, &Movement), With<Enemy>>,
     time: Res<Time>,
+    grid: Res<Grid>,
 ) {
     let player_transform = player_query.single();
     let player_translation = player_transform.translation.xy();
 
-    for (mut enemy_transform, movement) in enemies_query.iter_mut() {
-        let to_player = (player_translation - enemy_transform.translation.xy()).normalize();
-        enemy_transform.rotation = Quat::from_rotation_arc(Vec3::Y, to_player.extend(0.));
+    let player_square = world_to_grid_coords(player_translation);
 
-        if collide(
-            enemy_transform.translation,
-            Vec2::splat(16.),
-            player_transform.translation,
-            Vec2::splat(32.),
-        )
-        .is_none()
-        {
-            let forward = enemy_transform.up();
-            enemy_transform.translation += forward * time.delta_seconds() * movement.speed;
+    for (mut enemy_transform, mut path_cache, movement) in enemies_query.iter_mut() {
+        let enemy_square = world_to_grid_coords(enemy_transform.translation.truncate());
+
+        if enemy_square == player_square {
+            // If the enemy is close to the player, approach them in a straight line
+            if collide(
+                enemy_transform.translation,
+                Vec2::splat(16.),
+                player_transform.translation,
+                Vec2::splat(32.),
+            )
+            .is_none()
+            {
+                let direction =
+                    (enemy_transform.translation.truncate() - player_translation).normalize();
+
+                enemy_transform.translation +=
+                    (direction * movement.speed * time.delta_seconds()).extend(0.0);
+            }
+        } else {
+            // If the enemy is far away, use A* to traverse the map
+            if path_cache.player_square == player_square && path_cache.path.is_some() {
+                continue;
+            }
+
+            // Calculate the path and save it in cache
+            let path = astar(
+                &enemy_square,
+                |p| grid.neighbours(*p).into_iter().map(|p| (p, 1)),
+                |p| grid.distance(*p, player_square),
+                |p| *p == player_square,
+            );
+
+            if let Some(path) = path {
+                let path = path.0;
+
+                // Insert the path transformed from grid coords into world coords
+                path_cache.path = Some(
+                    path.into_iter()
+                        .map(|point| grid_to_world_coords(point))
+                        .collect(),
+                );
+
+                path_cache.index = 1;
+                path_cache.player_square = player_square;
+            }
+        }
+    }
+}
+
+fn update_enemy_position(
+    mut enemies_query: Query<(&mut Transform, &mut PathCache, &Movement), With<Enemy>>,
+    time: Res<Time>,
+) {
+    for (mut enemy_transform, mut path_cache, movement) in enemies_query.iter_mut() {
+        if let Some(path) = path_cache.path.as_ref() {
+            let direction = path[path_cache.index] - enemy_transform.translation.truncate();
+
+            let direction_normalized = direction.normalize_or_zero();
+
+            let movement_vector = direction_normalized * movement.speed * time.delta_seconds();
+
+            // If the next goal has been reached
+            if direction.length() <= movement_vector.length() {
+                enemy_transform.translation.x = path[path_cache.index].x;
+                enemy_transform.translation.y = path[path_cache.index].y;
+
+                path_cache.index += 1;
+
+                // The enemy finished the path
+                if path_cache.index >= path_cache.path.as_ref().unwrap().len() {
+                    path_cache.index = 0;
+                    path_cache.path = None;
+                    path_cache.player_square = (0, 0);
+                }
+
+                continue;
+            }
+
+            enemy_transform.translation += movement_vector.extend(0.0);
         }
     }
 }
 
 fn spawn_enemies(mut commands: Commands, textures: Res<TextureAssets>) {
-    for _ in 0..2 {
-        commands.spawn_bundle(EnemyBundle {
-            health: Health::new(100.),
-            movement: Movement { speed: 50. },
-            enemy: Enemy,
-            attack_timer: AttackTimer(Timer::from_seconds(2., true)),
-            sprite: SpriteBundle {
-                texture: textures.enemy_texture.clone(),
-                transform: Transform::from_translation(Vec3::new(-64., 0., 1.)),
-                ..default()
-            },
-        });
-    }
+    commands.spawn_bundle(EnemyBundle {
+        health: Health::new(100.),
+        movement: Movement { speed: 50.0 },
+        enemy: Enemy,
+        attack_timer: AttackTimer(Timer::from_seconds(2., true)),
+        path_cache: PathCache {
+            player_square: (12, 12),
+            path: None,
+            index: 0,
+        },
+        sprite: SpriteBundle {
+            texture: textures.enemy_texture.clone(),
+            transform: Transform::from_translation(Vec3::new(-250., 200., 1.)),
+            ..default()
+        },
+    }).insert(Name::new("Enemy"));
 }
 
 fn hit_player(
--- src/game_area.rs
+++ src/game_area.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use pathfinding::prelude::Grid;
 
 use crate::{
     loading::TextureAssets, GameState, GAME_AREA_HEIGHT, GAME_AREA_WIDTH, WALL_HEIGHT, WALL_WIDTH,
@@ -8,7 +9,8 @@ pub struct GameAreaPlugin;
 
 impl Plugin for GameAreaPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameState::Playing).with_system(wall_setup)).add_system_set(SystemSet::on_exit(GameState::Playing).with_system(drop_game_area));
+        app.add_system_set(SystemSet::on_enter(GameState::Playing).with_system(wall_setup))
+            .add_system_set(SystemSet::on_exit(GameState::Playing).with_system(drop_game_area));
     }
 }
 
@@ -21,9 +23,7 @@ fn wall_setup(mut commands: Commands, textures: Res<TextureAssets>) {
     let mut walls = Vec::new();
 
     // Spawn the walls at the top and bottom
-    for i in
-        -(GAME_AREA_WIDTH / WALL_WIDTH) as i32 / 2..(GAME_AREA_WIDTH / WALL_WIDTH) as i32 / 2
-    {
+    for i in -(GAME_AREA_WIDTH / WALL_WIDTH) as i32 / 2..(GAME_AREA_WIDTH / WALL_WIDTH) as i32 / 2 {
         // Top wall
         walls.push(
             commands
@@ -104,6 +104,48 @@ fn wall_setup(mut commands: Commands, textures: Res<TextureAssets>) {
         );
     }
 
+    let grid_size = ((GAME_AREA_WIDTH / WALL_WIDTH) as usize, (GAME_AREA_HEIGHT / WALL_HEIGHT) as usize);
+
+    let mut grid = Grid::new(grid_size.0, grid_size.1);
+
+    grid.enable_diagonal_mode();
+    grid.fill();
+
+    let inner_walls = vec![
+        (5, 5),
+        (6, 5),
+        (7, 5),
+        (5, 6),
+        (6, 6),
+        (7, 6),
+    ];
+
+    for wall in inner_walls {
+        grid.remove_vertex((wall.0, wall.1));
+
+        walls.push(
+            commands
+                .spawn_bundle(SpriteBundle {
+                    texture: wall_texture.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(
+                            wall.0 as f32 * WALL_WIDTH,
+                            wall.1 as f32 * WALL_HEIGHT,
+                            0.,
+                        ),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .insert(Name::new("Wall"))
+                .id(),
+        );
+    }
+
+    println!("{:?}", grid);
+
+    commands.insert_resource(grid);
+
     commands
         .spawn()
         // Later move it to an entity containing the entire game area (including floors and windows)
@@ -119,5 +161,34 @@ fn wall_setup(mut commands: Commands, textures: Res<TextureAssets>) {
 fn drop_game_area(mut commands: Commands, game_area: Query<Entity, With<GameArea>>) {
     for game_area in game_area.iter() {
         commands.entity(game_area).despawn_recursive();
+    }
+}
+
+pub fn world_to_grid_coords(world: Vec2) -> (usize, usize) {
+    (
+        ((GAME_AREA_WIDTH / 2. + world.x) / WALL_WIDTH) as usize,
+        ((GAME_AREA_HEIGHT / 2. - world.y ) / WALL_WIDTH) as usize,
+    )
+}
+
+pub fn grid_to_world_coords(grid: (usize, usize)) -> Vec2 {
+    Vec2::new(
+        grid.0 as f32 * WALL_WIDTH - GAME_AREA_WIDTH / 2.,
+        -(grid.1 as f32 * WALL_HEIGHT - GAME_AREA_HEIGHT / 2.),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::game_area::{grid_to_world_coords, world_to_grid_coords};
+
+    #[test]
+    fn coords_conversion() {
+        let coords = (3, 6);
+
+        assert_eq!(
+            world_to_grid_coords(grid_to_world_coords(coords)),
+            coords
+        );
     }
 }
