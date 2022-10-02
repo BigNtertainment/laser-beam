77a2b7d727385698d2aaa2802ea851c7085f4b79
Pietrek14 <dawidekpe@gmail.com>
2022-10-02 21:20:22 +0200

    it doesn't work :(

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,15 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        texture::BevyDefault,
+        view::RenderLayers,
+    }, sprite::{Material2d, MaterialMesh2dBundle},
+};
 
-#[derive(Component)]
-pub struct MainCamera;
+use crate::shaders::pixelise::PixeliseMaterial;
 
 pub struct CameraPlugin;
 
@@ -11,15 +19,147 @@ impl Plugin for CameraPlugin {
     }
 }
 
-fn camera_setup(mut commands: Commands) {
+#[derive(Component)]
+pub struct MainCamera;
+
+#[derive(Component)]
+pub struct Screen;
+
+#[derive(Deref, DerefMut)]
+pub struct PostProcessingLayer(pub RenderLayers);
+
+#[derive(Deref, DerefMut)]
+pub struct CameraRenderImage(pub Handle<Image>);
+
+#[derive(Deref, DerefMut)]
+pub struct ScreenRes(pub Entity);
+
+fn camera_setup(
+    mut commands: Commands,
+    windows: Res<Windows>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
+    mut post_processing_materials: ResMut<Assets<PixeliseMaterial>>,
+) {
+    let window = windows.primary();
+
+    let size = Extent3d {
+        width: window.width() as u32,
+        height: window.height() as u32,
+        ..Default::default()
+    };
+
+    // This is the texture that will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::bevy_default(),
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+        },
+        ..default()
+    };
+
+    // Fill image.data with zeroes
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    // Add main camera rendering to the image
     commands
         .spawn_bundle(Camera2dBundle {
             camera: Camera {
-                priority: 1,
+                priority: 0,
                 ..Default::default()
             },
             ..Default::default()
         })
-        .insert(Name::new("Camera"))
+        .insert(Name::new("MainCamera"))
         .insert(MainCamera);
+
+    // This specifies the layer used for the post processing camera, which will be attached to the post processing camera and 2d quad.
+    let post_processing_pass_layer = RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8);
+
+    let post_processing_pass_layer_resource = PostProcessingLayer(post_processing_pass_layer);
+
+    // This material has the texture that has been rendered.
+    let material_handle = post_processing_materials.add(PixeliseMaterial {
+        source_image: image_handle.clone(),
+    });
+
+    set_post_processing_effects(
+        &mut commands,
+        material_handle,
+        &windows,
+        &mut meshes,
+        &post_processing_pass_layer_resource,
+    );
+
+    commands.insert_resource(post_processing_pass_layer_resource);
+    commands.insert_resource(CameraRenderImage(image_handle));
+
+    // The post-processing pass camera.
+    commands
+        .spawn_bundle(Camera2dBundle {
+            camera: Camera {
+                // renders after the first main camera which has default value: 0.
+                priority: 1,
+                ..default()
+            },
+            ..Camera2dBundle::default()
+        })
+        .insert(post_processing_pass_layer)
+        .insert(Name::new("SecondaryCamera"));
+}
+
+fn set_post_processing_effects<M: Material2d>(
+	commands: &mut Commands,
+	material: Handle<M>,
+    windows: &Res<Windows>,
+	meshes: &mut ResMut<Assets<Mesh>>,
+	post_processing_pass_layer: &PostProcessingLayer,
+) {
+    let window = windows.primary();
+
+    let size = Vec2::new(
+        window.width(),
+        window.height(),
+    );
+
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(size)));
+
+	// Post processing 2d quad, with material using the render texture done by the main camera, with a custom shader.
+	let screen = commands
+		.spawn_bundle(MaterialMesh2dBundle {
+			mesh: quad_handle.into(),
+			material,
+			transform: Transform {
+				translation: Vec3::new(0.0, 0.0, 1.5),
+				..default()
+			},
+			..default()
+		})
+		.insert(post_processing_pass_layer.0)
+		.insert(Name::new("Screen"))
+		.insert(Screen).id();
+
+	commands.insert_resource(ScreenRes(screen));
+}
+
+pub fn update_post_processing_effects<M: Material2d>(
+	commands: &mut Commands,
+	screen: &Entity,
+	material: Handle<M>,
+    windows: &Res<Windows>,
+	meshes: &mut ResMut<Assets<Mesh>>,
+	post_processing_pass_layer: &PostProcessingLayer,
+) {
+	commands.entity(*screen).despawn_recursive();
+
+	set_post_processing_effects(commands, material, windows, meshes, post_processing_pass_layer);
 }
